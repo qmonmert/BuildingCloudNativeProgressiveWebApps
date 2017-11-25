96f4c0a57e82e7d39023edd363fa44e75a14cb1c
Quentin Monmert <qmonmert@mbp-de-cedric.home>
2017-11-25 19:55:13 +0100

    Add eureka-service and edge-service

--- bike-catalog-service/src/main/kotlin/fr/ippon/bikecatalogservice/BikeCatalogServiceApplication.kt
+++ bike-catalog-service/src/main/kotlin/fr/ippon/bikecatalogservice/BikeCatalogServiceApplication.kt
@@ -3,17 +3,17 @@ package fr.ippon.bikecatalogservice
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.context.support.beans
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.body
 import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.Flux
 
+@EnableDiscoveryClient
 @SpringBootApplication
 class BikeCatalogServiceApplication
 
@@ -34,7 +34,7 @@ fun main(args: Array<String>) {
                 bean {
                     ApplicationRunner {
                         val bikeRepository = ref<BikeRepository>()
-                        val bikes = Flux.just("Trek", "Canyon", "Giant")
+                        val bikes = Flux.just("Trek SL5", "Canyon", "Giant", "Trek SL6")
                                 .map { Bike(name = it) }
                                 .flatMap { bikeRepository.save(it) }
                         bikeRepository
@@ -48,12 +48,6 @@ fun main(args: Array<String>) {
             })
             .run(*args)
 }
-
-//@RestController
-//class BikeRestController(private val bikeRepository: BikeRepository) {
-//    @GetMapping("/bikes")
-//    fun bikes() = this.bikeRepository.findAll()
-//}
 
 interface BikeRepository : ReactiveMongoRepository<Bike, String>
 
---
+++ edge-service/src/main/kotlin/fr/ippon/edgeservice/EdgeServiceApplication.kt
@@ -0,0 +1,71 @@
+package fr.ippon.edgeservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancerExchangeFilterFunction
+import org.springframework.cloud.gateway.route.gateway
+import org.springframework.cloud.gateway.handler.predicate.RoutePredicates.path
+import org.springframework.cloud.netflix.hystrix.HystrixCommands
+import org.springframework.context.support.beans
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.body
+import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.Flux
+
+@EnableDiscoveryClient
+@SpringBootApplication
+class EdgeServiceApplication
+
+fun main(args: Array<String>) {
+
+    SpringApplicationBuilder()
+            .sources(EdgeServiceApplication::class.java)
+            .initializers(
+                beans {
+
+                    bean {
+                        val lbFilter = ref<LoadBalancerExchangeFilterFunction>()
+                        WebClient.builder().filter(lbFilter).build()
+                    }
+
+                    bean {
+                        gateway {
+                            route {
+                                id("bike-catalog-service")
+                                predicate(path("/bike-catalog-service") or path("/original-bikes"))
+                                uri("lb://bike-catalog-service/bikes")
+                            }
+                        }
+                    }
+
+                    bean {
+                        router {
+                            val client = ref<WebClient>()
+                            GET("/trek-bikes") {
+                                val otherBikes = listOf("Canyon", "Giant")
+
+                                val trekBikes = client.get().uri("http://bike-catalog-service/bikes")
+                                        .retrieve()
+                                        .bodyToFlux(Bike::class.java)
+                                        .filter { bike -> !otherBikes.contains(bike.name) }
+
+                                val failureReadyBikes = HystrixCommands
+                                        .from(trekBikes)
+                                        .commandName("bikes")
+                                        .fallback(Flux.empty())
+                                        .eager()
+                                        .build()
+
+                                ServerResponse.ok().body(failureReadyBikes)
+                            }
+                        }
+                    }
+
+                }
+            )
+            .run(*args)
+}
+
+data class Bike(var id: String? = null, var name: String? = null)
