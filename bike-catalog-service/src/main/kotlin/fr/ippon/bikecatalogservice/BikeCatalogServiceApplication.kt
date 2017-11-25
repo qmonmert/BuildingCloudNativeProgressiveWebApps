4f38a9424083ad7efd631d2aa0491a929da6dc7b
Quentin Monmert <qmonmert@pc-31.home>
2017-11-25 17:47:05 +0100

    Rewrite main function

--- bike-catalog-service/src/main/kotlin/fr/ippon/bikecatalogservice/BikeCatalogServiceApplication.kt
+++ bike-catalog-service/src/main/kotlin/fr/ippon/bikecatalogservice/BikeCatalogServiceApplication.kt
@@ -1,49 +1,38 @@
 package fr.ippon.bikecatalogservice
 
-import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.boot.runApplication
 import org.springframework.context.support.beans
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
-import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
 
 @SpringBootApplication
 class BikeCatalogServiceApplication
 
 fun main(args: Array<String>) {
-    runApplication<BikeCatalogServiceApplication>(*args)
-}
+    SpringApplicationBuilder()
+            .sources(BikeCatalogServiceApplication::class.java)
+            .initializers(beans {
 
-//fun main(args: Array<String>) {
-//    SpringApplicationBuilder()
-//            .sources(BikeCatalogServiceApplication::class.java)
-//            .initializers(beans {
-//
-//            })
-//            .run(*args)
-//}
+                bean {
+                    ApplicationRunner {
+                        val bikeRepository = ref<BikeRepository>()
+                        val bikes = Flux.just("Trek", "Canyon", "Giant")
+                                .map { Bike(name = it) }
+                                .flatMap { bikeRepository.save(it) }
+                        bikeRepository
+                                .deleteAll()
+                                .thenMany(bikes)
+                                .thenMany(bikeRepository.findAll())
+                                .subscribe { println(it) }
+                    }
+                }
 
-@Component
-class SampleBikeRepository(private val bikeRepository : BikeRepository) : ApplicationRunner {
-
-    override fun run(args: ApplicationArguments?) {
-
-        val bikes = Flux.just("Trek", "Canyon", "Giant")
-                .map { Bike(name = it) }
-                .flatMap { bikeRepository.save(it) }
-
-        bikeRepository
-                .deleteAll()
-                .thenMany(bikes)
-                .thenMany(bikeRepository.findAll())
-                .subscribe { println(it) }
-    }
-
+            })
+            .run(*args)
 }
 
 interface BikeRepository : ReactiveMongoRepository<Bike, String>
