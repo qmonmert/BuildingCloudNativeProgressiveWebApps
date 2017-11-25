32fa2c7a1437f1ab2fac346e04551d2cd72397f0
Quentin Monmert <qmonmert@pc-31.home>
2017-11-25 18:03:14 +0100

    Add route

--- bike-catalog-service/src/main/kotlin/fr/ippon/bikecatalogservice/BikeCatalogServiceApplication.kt
+++ bike-catalog-service/src/main/kotlin/fr/ippon/bikecatalogservice/BikeCatalogServiceApplication.kt
@@ -7,6 +7,11 @@ import org.springframework.context.support.beans
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.body
+import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.Flux
 
 @SpringBootApplication
@@ -16,6 +21,15 @@ fun main(args: Array<String>) {
     SpringApplicationBuilder()
             .sources(BikeCatalogServiceApplication::class.java)
             .initializers(beans {
+
+                bean {
+                    val bikeRepository = ref<BikeRepository>()
+                    router {
+                        GET("/bikes") {
+                            ServerResponse.ok().body(bikeRepository.findAll())
+                        }
+                    }
+                }
 
                 bean {
                     ApplicationRunner {
@@ -34,6 +48,12 @@ fun main(args: Array<String>) {
             })
             .run(*args)
 }
+
+//@RestController
+//class BikeRestController(private val bikeRepository: BikeRepository) {
+//    @GetMapping("/bikes")
+//    fun bikes() = this.bikeRepository.findAll()
+//}
 
 interface BikeRepository : ReactiveMongoRepository<Bike, String>
 
